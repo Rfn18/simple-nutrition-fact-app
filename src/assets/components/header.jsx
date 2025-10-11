6ddc8c197f5a41f60ff0be72394b6dcd343dac40
Fasterino <rinofaster89@gmail.com>
2025-10-11 18:36:07 +0700

    styling ketika fetching sudah selesai

--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <p className="text-center">
           Enter an ingredient list list for what you are cooking, like
           <span className="text-amber-500">"1 cup rice, 10 oz chickpeas"</span>,
-          etc. Enter each ingredient on a new line.
+          etc. <br /> Enter each ingredient on a new line.
         </p>
       </header>
     </>
--- src/assets/components/main.jsx
+++ src/assets/components/main.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 
-function Main({handleLoading}) {
+function Main({ handleLoading }) {
   const [food, setFood] = useState([]);
   const [newFood, setNewFood] = useState("");
   const [data, setData] = useState([]);
 
   function addFood() {
-      if (newFood.trim() !== "") {
-        setFood((t) => [...t, newFood]);
-        setNewFood("");
-      }
+    if (newFood.trim() !== "") {
+      setFood((t) => [...t, newFood]);
+      setNewFood("");
+    }
   }
 
   function enterDown(e) {
@@ -23,17 +23,17 @@ function Main({handleLoading}) {
       const lastLength = food.length - 1;
       const testData = food[lastLength].split("\n");
 
-    const fetching = async () => {
-          handleLoading(true)
-          try {
-           await new Promise(t => setTimeout(t, 1000))
-          } catch (err) {
-            console.error()
-          } finally {
-            handleLoading(false)
-          }
-      }
-      fetching()
+      const fetching = async () => {
+        handleLoading(true);
+        try {
+          await new Promise((t) => setTimeout(t, 1000));
+        } catch (err) {
+          console.error();
+        } finally {
+          handleLoading(false);
+        }
+      };
+      fetching();
 
       // async function apiEdamam() {
       //   const app_id = "86a7058f";
@@ -56,20 +56,162 @@ function Main({handleLoading}) {
     }
   }, [food]);
 
-  return (
-    <>
-      <main className="flex flex-col items-center w-full h-full">
-          <textarea
+  function MainData() {
+    return (
+      <>
+        <textarea
           name="inputArea"
           id="inpText"
           className="w-[80%] h-[40dvh] border-2 border-[#eaeaea] bg-white rounded-md focus:outline-0 p-3 text-md "
           onChange={(e) => setNewFood(e.target.value)}
-          onKeyDown={enterDown} 
+          onKeyDown={enterDown}
           placeholder={`1 cup rice,\n10 oz chickpeas`}
+        ></textarea>
+        <button
+          className="bg-[#fab12f] text-white font-bold rounded-4xl py-[.5em] px-[2em] cursor-pointer transition ease-in scale-100 hover:bg-white hover:text-[#fab12f] hover:scale-95 btnAnalyze"
+          onClick={addFood}
+        >
+          Analyze
+        </button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <main className="flex justify-between items-center w-full h-full mt-[2em] px-10">
+        {/* <MainData /> */}
+        <div className="flex flex-col justify-center items-center gap-5 w-2xl containerItems">
+          <textarea
+            name="inputArea"
+            id="inpText"
+            className="w-[80%] h-[40dvh] border-2 border-[#ddd] bg-white rounded-md focus:outline-0 p-3 text-md "
+            onChange={(e) => setNewFood(e.target.value)}
+            onKeyDown={enterDown}
+            placeholder={`1 cup rice,\n10 oz chickpeas`}
           ></textarea>
-          <button className="bg-[#fab12f] text-white font-bold rounded-4xl py-[.5em] px-[2em] cursor-pointer transition ease-in scale-100 hover:bg-white hover:text-[#fab12f] hover:scale-95 btnAnalyze" onClick={addFood}>
-            Analyze
-          </button>
+          <div className="flex gap-3 container-button">
+            <button
+              className="bg-[#fab12f] text-white font-bold text-sm rounded-4xl py-[.5em] px-[20px] w-30 cursor-pointer transition ease-in scale-100 hover:bg-white hover:text-[#fab12f] hover:scale-95 btnAnalyze"
+              onClick={addFood}
+            >
+              Analyze
+            </button>
+            <button className="bg-[#fab12f] text-white font-bold text-sm rounded-4xl py-[.5em] px-[20px] w-30 cursor-pointer transition ease-in scale-100 hover:bg-white hover:text-[#fab12f] hover:scale-95 btnAnalyze">
+              New recipe
+            </button>
+          </div>
+          <div className="w-[80%] border-2 border-[#ddd] bg-white rounded-md container-ingridients">
+            <table className="justify-self-center w-full">
+              <thead className="">
+                <th>Qty</th>
+                <th>Unit</th>
+                <th>Food</th>
+                <th>Calories</th>
+                <th>Weight</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Cup</td>
+                  <td>Rice</td>
+                  <td>200</td>
+                  <td>230g</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Cup</td>
+                  <td>Rice</td>
+                  <td>200</td>
+                  <td>230g</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="w-60 bg-white container-card-nutrition">
+          <h1>Nutrition Facts</h1>
+          <table>
+            <thead>
+              <th>Amount Per Serving</th>
+            </thead>
+            <tr>
+              <th>
+                <b>Calories</b>
+              </th>
+              <td>1174</td>
+            </tr>
+            <tr className="thick-row">
+              <td>
+                <d>% Daily Value*</d>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <b>
+                  Total Fat <span>18.3 g</span>
+                </b>
+              </th>
+              <td>28%</td>
+            </tr>
+            <tr>
+              <th>
+                Saturated Fat <span>2 g</span>
+              </th>
+              <td>10%</td>
+            </tr>
+            <tr>
+              <th>
+                Trans Fat <span>0 g</span>
+              </th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>
+                <b>
+                  Cholesterol <span>0 mg</span>
+                </b>
+                <td>0 %</td>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <b>
+                  Sodium <span>70 mg</span>
+                </b>
+                <td>3 %</td>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <b>
+                  Total Carbohydrate <span>333.2 g</span>
+                </b>
+                <td>111 %</td>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                Dietary Fiber <span>34.6 g</span>
+                <td>138 %</td>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                Total Sugars <span>30.3 g</span>
+                <td></td>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <b>
+                  Protein <span>71 g</span>
+                </b>
+                <td>142 %</td>
+              </th>
+            </tr>
+          </table>
+        </div>
       </main>
     </>
   );
