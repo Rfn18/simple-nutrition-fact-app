80cf7f691ca3e2024294a5c7d58087ead6f90ae3
Rfn18 <rinofaster89@gmail.com>
2025-10-16 18:51:15 +0700

    update bactesting

--- src/assets/components/main.jsx
+++ src/assets/components/main.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import MainData from "./mainData";
 import MainResult from "./mainResult";
 
+import dotenv from "dotenv";
+
 function Main({ handleLoading }) {
   const [food, setFood] = useState([]);
   const [newFood, setNewFood] = useState("");
@@ -43,8 +45,8 @@ function Main({ handleLoading }) {
                 {
                   method: "POST",
                   headers: {
-                    "x-app-id": "2d3e9978",
-                    "x-app-key": "30d9676fca4af72866e0ada43f530f25",
+                    "x-app-id": import.meta.env.VITE_APP_ID,
+                    "x-app-key": import.meta.env.VITE_APP_KEY,
                     "Content-Type": "application/json",
                   },
                   body: JSON.stringify({ query: item.toLocaleString() }),
@@ -74,7 +76,7 @@ function Main({ handleLoading }) {
   const styles = () => {
     return load === false ? "flex " : "hidden " 
   }
-
+  
   return (
     <>
       <main className={styles() + "flex-col items-center gap-4 w-full h-full mt-[2em] px-10 mainContainer"}>
