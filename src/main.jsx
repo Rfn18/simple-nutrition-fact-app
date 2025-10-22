7c100fb2939af2ce851ea817e661cac281268d8a
Rfn18 <rinofaster89@gmail.com>
2025-10-22 23:15:57 +0700

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
 import './index.css'
 import App from './App.jsx'
+import Menu from './assets/components/menu.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }, {
+    path: "/menu",
+    element: <Menu />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />,
   </StrictMode>,
 )
