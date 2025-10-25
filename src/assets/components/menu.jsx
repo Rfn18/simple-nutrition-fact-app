582ca9fe73171d12c02f69465586cbcaad1fffb6
Rfn18 <rinofaster89@gmail.com>
2025-10-25 16:14:06 +0700

    update commit

--- src/assets/components/menu.jsx
+++ src/assets/components/menu.jsx
@@ -3,16 +3,16 @@ function Menu() {
     return (
         <>
             <Headers />
-            <main className="h-[60vh] w-full flex flex-col items-center mt-10">
-                <h1 className="font-bold text-3xl ">Menu</h1>
+            <main className="h-[60vh] w-full flex flex-col items-center mt-5">
+                <h1 className="font-bold text-3xl mb-4">Menu</h1>
                 <div className="flex gap-5 items-center justify-center container-menu">
-                    <div className="flex flex-col items-center photos">
+                    <div className="flex flex-col justify-center items-center cursor-pointer hover:bg-amber-100 hover:text-[#fab12f] transition ease-in bg-[#fab12f] rounded text-white p-4 w-[10em] h-[7em] photos">
                           <i className="fa-solid fa-camera text-2xl"></i>
                           <p>Scan Your Food</p>
                     </div>
-                    <div className="flex flex-col items-center search">
+                    <div className="flex flex-col justify-center items-center cursor-pointer  hover:bg-amber-100 hover:text-[#fab12f] transition ease-in bg-[#fab12f] rounded text-white p-4 w-[10em] h-[7em] search">
                         <i className="fa-solid fa-magnifying-glass text-2xl"></i>
-                        <p>Search Foods Nutrition</p>
+                        <p className="text-center">Search Foods</p>
                     </div>
                 </div>
             </main>
